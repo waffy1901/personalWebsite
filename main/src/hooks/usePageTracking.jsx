0fe9db92bf13aa2da12a2decc4d84f64951c6ef4
waffy1901 <waffyahmed@gmail.com>
2026-04-19 17:17:46 -0400

    CRA -> Vite Migration

--- main/src/hooks/usePageTracking.jsx
+++ main/src/hooks/usePageTracking.jsx
@@ -3,12 +3,14 @@ import { useLocation } from "react-router-dom";
 
 export default function usePageTracking() {
   const location = useLocation()
+  const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID
+
   useEffect(() => {
-    if (window.gtag) {
+    if (window.gtag && measurementId) {
       window.gtag("event", "page_view", {
         page_path: location.pathname + location.search,
-        send_to: process.env.REACT_APP_GA_MEASUREMENT_ID,
+        send_to: measurementId,
       });
     }
-  }, [location]);
+  }, [location, measurementId]);
 }
