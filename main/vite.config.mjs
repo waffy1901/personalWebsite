a6891e3a28cbf7ab5f3379ad7b4c0445f6c6441f
waffy1901 <waffyahmed@gmail.com>
2026-04-19 17:35:03 -0400

    fix duplication

--- main/vite.config.mjs
+++ main/vite.config.mjs
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2019',
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
